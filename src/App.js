cd57a6fe81b6344c4bb7a83540ced3f2c70eba63
devnoman01 <devnoman01@gmail.com>
2022-04-04 00:44:18 +0600

    utility library installation complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img
+          src="https://gadgetnmusic.com/wp-content/uploads/2021/10/2021101513112716342746879424.png"
+          className="App-logo"
+          style={{ width: "800px" }}
+          alt="logo"
+        />
+        <h1>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
